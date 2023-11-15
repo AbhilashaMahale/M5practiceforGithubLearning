851c8e5e6f4d2b283c3487a158b92952dea84bc7
abhilasha <abhilasha@LAPTOP-2LV7AID1>
2023-11-15 11:28:13 +0530

    Demo class pushed

---
+++ src/test/java/GitHUBLearn/Practice1.java
@@ -0,0 +1,33 @@
+package GitHUBLearn;
+
+public class Practice1 {
+
+	public static void main(String[] args) {
+
+
+		
+		System.out.println("Welcome to Github Learning abhilasha, Enjoy");
+		int a = 5;
+		int b = 10;
+		
+		int c = a+b;
+		int d = a*b;
+		
+		if(c>d) 
+		{
+			System.out.println("Addition Wins");
+			
+		}
+		else
+        {   
+			System.out.println("Addition Lost and Multiplication Wins");
+			System.out.println("Value of C is ="+c);			
+			System.out.println("Value of D is ="+d);
+		}
+		  
+		
+		System.out.println("Good Job well done Ab");
+
+	}
+
+}
