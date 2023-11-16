fa6cf456b0327c3a265bee2c1eb16900bc497938
abhilasha <abhilasha@LAPTOP-2LV7AID1>
2023-11-16 10:55:56 +0530

    push this data to check for pull request

---
+++ src/main/java/ForPULLrequestExample/Utis.java
@@ -0,0 +1,9 @@
+package ForPULLrequestExample;
+
+public class Utis {
+	
+	public void pullrequestexample() {
+	System.out.println(" PULL this data from Framework developer workspace to AT workspace ");
+	}
+
+}
