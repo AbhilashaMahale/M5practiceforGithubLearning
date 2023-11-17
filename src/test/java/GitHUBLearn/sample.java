e2c5ff58943032d78250380a1a77ba34b7679c0f
abhilasha <abhilasha@LAPTOP-2LV7AID1>
2023-11-17 10:57:22 +0530

    ok

---
+++ src/test/java/GitHUBLearn/sample.java
@@ -0,0 +1,9 @@
+package GitHUBLearn;
+
+public class sample {
+	
+	public static void main(String[] args) {
+		System.out.println("TC 001 its written by ATW to push the request with etst case iD");
+	}
+
+}
